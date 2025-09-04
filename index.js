fe583ab99a99f6c2b6414f958d4c2ebd53a7ce4d
fawaz raza <150109036+AbdulMoeez1278@users.noreply.github.com>
2025-09-05 01:58:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
 const express = require("express");
 
+// importing your data
+const data = require("./data");
+const jsonData = require("./jsonData");
+
 const app = express();
 
 const port = 3000;
 const hostname = "127.0.0.1";
 
-app.get("/users", (req, res) => {
-  res.send("Getting data related to the users");
-  res.status(500).send("Something broke!");
+// application level router
+const verification = (req, res, next) => {
+  const age = parseInt(req.query.age); // Read age from query param
+
+  if (isNaN(age)) {
+    return res.status(400).send("Age is required and must be a number.");
+  }
+
+  if (age < 18) {
+    return res.status(403).send("Access denied: User must be 18 or older.");
+  }
+
+  if (age > 45) {
+    return res
+      .status(400)
+      .send(
+        `Access denied: User's age ${req.query.age} is greater then the following age`
+      );
+  }
+
+  // Age is valid, proceed to route handler
+  next();
+};
+
+app.get("/", (req, res) => {
+  res.send("Data are getting fetched!");
+  console.log("Data is trying to be fetched");
 });
 
-app.get("/admin", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: "Moeez",
-      email: "moeez@gmail.com",
-      city: "Lahore",
-    },
-    {
-      id: 2,
-      name: "Rameez",
-      email: "rameez@gmail.com",
-      city: "Dubai",
-    },
-    {
-      id: 3,
-      name: "Bilal",
-      email: "bilal@gmail.com",
-      city: "Haroonabad",
-    },
-    {
-      id: 4,
-      name: "Hamza",
-      email: "hamza@gmail.com",
-      city: "Lahore",
-    },
-    {
-      id: 5,
-      name: "Sohaib",
-      email: "sohaib@gmail.com",
-      city: "Lahore",
-    },
-  ]);
+app.get("/users", (req, res) => {
+  res.json(data);
+  console.log("Getting data related to the users");
+});
+
+app.get("/profile", (req, res) => {
+  res.send("<h1>Profile for Different users</h1>");
+  console.log("Profile for different users");
+});
+
+app.get("/admin", verification, (req, res) => {
+  res.json(jsonData);
+  console.log("Data is stored in the JSON form");
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server is listening on localhost:${port}`);
+  console.log(`Server is listening on https://${hostname}:${port}`);
 });
---
+++ jsonData.js
@@ -0,0 +1,64 @@
+const jsonData = [
+    {
+      "id": 1,
+      "name": "Moeez",
+      "email": "moeez@gmail.com",
+      "city": "Lahore"
+    },
+    {
+      "id": 2,
+      "name": "Rameez",
+      "email": "rameez@gmail.com",
+      "city": "Dubai"
+    },
+    {
+      "id": 3,
+      "name": "Bilal",
+      "email": "bilal@gmail.com",
+      "city": "Haroonabad"
+    },
+    {
+      "id": 4,
+      "name": "Hamza",
+      "email": "hamza@gmail.com",
+      "city": "Lahore"
+    },
+    {
+      "id": 5,
+      "name": "Sohaib",
+      "email": "sohaib@gmail.com",
+      "city": "Lahore"
+    },
+    {
+      "id": 6,
+      "name": "Rizwan",
+      "email": "rizwan@gmail.com",
+      "city": "Lahore"
+    },
+    {
+      "id": 7,
+      "name": "Nouman",
+      "email": "nouman@gmail.com",
+      "city": "Lahore"
+    },
+    {
+      "id": 8,
+      "name": "Naveed",
+      "email": "naveed@gmail.com",
+      "city": "Lahore"
+    },
+    {
+      "id": 9,
+      "name": "Abdullah",
+      "email": "abdullah@gmail.com",
+      "city": "Lahore"
+    },
+    {
+      "id": 10,
+      "name": "Zaid",
+      "email": "zaid@gmail.com",
+      "city": "Lahore"
+    }
+];
+
+  module.exports = jsonData;

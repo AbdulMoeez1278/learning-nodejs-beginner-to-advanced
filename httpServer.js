c17ccd0c77a49b05fe6fef45789c6db28af83b0e
Abdul Moeez <abdulmoeez1278@gmail.com>
2025-09-18 01:51:08 +0500

    Update httpServer.js

--- httpServer.js
+++ httpServer.js
@@ -2,30 +2,62 @@
 const http = require("http"); // include http module
 
 // creating a server using an http
-const server1 = http.createServer((req, res) => {
+const serverOne = http.createServer((req, res) => {
   res.write("<h1>Creating http server</h1>");
-  res.end("Hello World");
   console.log("PORT: 3000");
 
+  // res.end("Hello World");
   //  res.writeHead(200, { "Content-Type": "text/plain" });
 });
 
 // server listening
-server1.listen(3000, () => {
+serverOne.listen(3000, () => {
   console.log("Server 1 is listening on port 3000");
 });
 
 // Server 2
-const server2 = http.createServer((req, res) => {
+const serverTwo = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello from Server 2!\n");
   console.log("PORT: 3001");
 });
 
-server2.listen(3001, () => {
+serverTwo.listen(3001, () => {
   console.log("Server 2 is listening on port 3001");
 });
 
+// Server 3
+const serverThree = http.createServer((req, res) => {
+  res.end("Hello from Server 3!");
+  console.log("PORT: 3002");
+
+  // res.writeHead(200, { "Content-Type": "text/plain" });
+});
+
+serverThree.listen(3002, () => {
+  console.log("Server 3 is listening on port 3002");
+});
+
+// Server 4
+const serverFour = http.createServer((req, res) => {
+  res.end("Server 4 is running perfectly!");
+  console.log("PORT: 3003");
+});
+
+serverFour.listen(3003, () => {
+  console.log("Server 4 is listening on port 3003");
+});
+
+// Server 5
+const serverFive = http.createServer((req, res) => {
+  res.end("Server 5 is running perfectly!");
+  console.log("PORT: 3004");
+});
+
+serverFive.listen(3004, () => {
+  console.log("Server 5 is listening on port 3004");
+});
+
 // interview question
-// Can we create two servers in same file?
-// Yes, it is possible to create and run two HTTP servers within the same Node.js file. Each server must listen on a different port to avoid port conflicts.
+// Can we create two || multiple servers in same file?
+// Yes, it is possible to create and run two || multiple HTTP servers within the same Node.js file. Each server must listen on a different port to avoid port conflicts.

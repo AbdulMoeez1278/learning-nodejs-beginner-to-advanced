150a866fa0a06252cf9995508dc4628f31b7cee8
Abdul Moeez <abdulmoeez1278@gmail.com>
2025-09-24 13:11:16 +0500

    Update syncAsync.js

--- syncAsync.js
+++ syncAsync.js
@@ -21,13 +21,14 @@ console.log("Asynchronous in Node.js");
 
 // Blocking code example
 console.log("Start of blocking code");
-const dataFile = fs.readFileSync("myfile.txt", "utf8"); // Blocks here
+const dataFile = fs.readFileSync("file.txt", "utf8"); // Blocks here
 console.log("Blocking operation completed");
 
 // Non-blocking code example
 console.log("Start of non-blocking code");
 fs.readFile("myfile.txt", "utf8", (err, data) => {
-  if (err) throw err;
-  console.log("Non-blocking operation completed");
+  if (err) {
+    console.log("Non-blocking operation completed");
+  }
 });
 console.log("This runs before the file is read");

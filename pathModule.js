f91c8f3b6a9fac72237223218da249719183d365
Abdul Moeez <abdulmoeez1278@gmail.com>
2025-09-26 18:54:18 +0500

    Update pathModule.js

--- pathModule.js
+++ pathModule.js
@@ -25,3 +25,14 @@ console.log("Extension name is:", extension);
 // path.resolve(): Resolves a sequence of paths or path segments into an absolute path
 const absolutePath = path.resolve("fileSystemCRUD", "textMain.txt");
 console.log("Absolute Path is:", absolutePath);
+
+// Global Constants...
+// 1. directory name
+console.log("Directory name is:", __dirname);
+
+// 2. file name
+console.log("File name is:", __filename);
+
+// 3. relationship
+const filePath = path.join(__dirname, "mainText.txt");
+console.log(filePath);

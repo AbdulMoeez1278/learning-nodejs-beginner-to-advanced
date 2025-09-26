e9388a990909f1e85a4b852d9cf9b6457f3adeb2
Abdul Moeez <abdulmoeez1278@gmail.com>
2025-09-26 14:04:34 +0500

    Create pathModule.js

---
+++ pathModule.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+// path.join(): Joins all given path segments together, normalizing the resulting path.
+const fullPath = path.join(__dirname, "/fileSystemCRUD", "mainText.txt");
+console.log("Full Path is:", fullPath);
+
+// path.dirname(): Returns the directory name of a path.
+const dirName = path.dirname(
+  "/GitHub/learning-nodejs-beginner-to-advanced/fileSystemCRUD/mainText.txt"
+);
+console.log("Directory name is:", dirName);
+
+// path.basename(): Returns the last portion of a path.
+const fileName = path.basename(
+  "/learning-nodejs-beginner-to-advanced/fileSystemCRUD/mainText.txt"
+);
+console.log("File name is:", fileName);
+
+// path.extname(): Returns the extension of a path.
+const extension = path.extname(
+  "/GitHub/learning-nodejs-beginner-to-advanced/fileSystemCRUD/mainText.txt"
+);
+console.log("Extension name is:", extension);
+
+// path.resolve(): Resolves a sequence of paths or path segments into an absolute path
+const absolutePath = path.resolve("fileSystemCRUD", "textMain.txt");
+console.log("Absolute Path is:", absolutePath);

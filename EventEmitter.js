f94f9f650081b492323ccca2dc8840c51af972a0
Abdul Moeez <abdulmoeez1278@gmail.com>
2025-11-18 00:44:30 +0500

    Update EventEmitter.js

--- EventEmitter.js
+++ EventEmitter.js
@@ -10,3 +10,35 @@ emitter.on("greet", () => {
 
 // 2. Emit the event
 emitter.emit("greet");
+
+// 3. Using once()
+emitter.once("start", () => {
+  console.log("This will run only once!");
+});
+
+emitter.emit("start");
+emitter.emit("start"); // ignored
+
+// 4. Remove event listeners
+const greet = () => console.log("Hi");
+
+emitter.on("greet", greet);
+emitter.off("greet", greet); // remove listener
+
+// 5. Advanced Use: Create your own class with Evemts
+const EventEmitter = require("events");
+
+class Chat extends EventEmitter {
+  sendMessage(msg) {
+    console.log("Sending:", msg);
+    this.emit("message", msg);
+  }
+}
+
+const chat = new Chat();
+
+chat.on("message", (msg) => {
+  console.log("New Message:", msg);
+});
+
+chat.sendMessage("Hello Moizy!");
